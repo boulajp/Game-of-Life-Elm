8453e9290ac4f40b9280d0865bec327b841fc103
Jonathan Boulanger <boulajp@mcmaster.ca>
2015-06-20 18:15:52 -0400

    added game of life and gameboard

---
+++ gameboard.elm
@@ -0,0 +1,18 @@
+{-
+    This board should be an nxn list 
+-}
+
+
+board : List (List Bool)
+board = 
+    [
+        [False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+       ,[False,False,False,False,False,False,False,False,False]
+    ]
