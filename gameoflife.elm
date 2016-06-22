dbea5293cedb2c16d33c3a9803649b45d3a668b3
Jon Boulanger <boulajp@mcmaster.ca>
2016-06-22 00:20:56 -0400

    Update gameoflife.elm

--- gameoflife.elm
+++ gameoflife.elm
@@ -9,33 +9,28 @@ import Maybe
 
 -- Model
 type alias GameState = List (List Bool)
+type alias Point = (Int, Int)
+type alias Cell = (Point, Bool)
+type alias Board = List (List Cell)
 
 -- Update
 step : Float -> GameState -> GameState
 step t gameState =
   let
     -- Map the board indexes to each position
-    gameStateIndexes : List (List ((Int,Int), Bool))
+    gameStateIndexes : Board
     gameStateIndexes = List.map (\(y,l) -> (List.map (\(x,e) -> ((x,y), e)) l)) (List.indexedMap (,) (List.map (List.indexedMap (,)) gameState)) -- [[((0,0),False)],[((0,1), False)]]
     -- Create a function to tell whether a given cell will live
-    cellLives : List (List ((Int,Int), Bool)) -> (Int,Int) -> Bool
+    cellLives : Board -> Point -> Bool
     cellLives boardList (x,y) =
       let 
-        -- This function takes boardList and and (x,y) and returns the index of the list at (x,y)
         getAtXY l (x,y) = (head (drop y l)) `Maybe.andThen` (\a -> (head (drop x a))) |> Maybe.withDefault ((0,0),False)
-        -- Create a list of neigbours relative to a position
         neighbours = [(x-1,y-1), (x,y-1), (x+1,y-1), (x-1,y), (x+1, y), (x-1,y+1), (x,y+1), (x+1,y+1)]
-        -- Map the list of neigbours with whether or not that position is on
-        neighboursAlive = (map (getAtXY boardList >> snd) neighbours)
-        -- Convert the list from Bool to Int
-        neighboursAliveInt = map (\a -> if a then 1 else 0) neighboursAlive
-        -- Get the sum of the list
-        numAliveNeighbours = sum neighboursAliveInt
-        -- Get whether the current cell is alive
+        numAliveNeighbours = sum <| map (\a -> if a then 1 else 0) (map (getAtXY boardList >> snd) neighbours)
         curCellAlive = snd (getAtXY boardList (x,y))
         -- This function returns whether a cell lives based on its alive neighbours and whether it is already alive
-        cellRules : Int -> Bool -> Bool
-        cellRules aliveNeighbours isAlive =
+        willCellLive : Int -> Bool -> Bool
+        willCellLive aliveNeighbours isAlive =
           if isAlive
           then 
               if
@@ -45,13 +40,13 @@ step t gameState =
           else
               aliveNeighbours == 3
      in
-       -- Return whether the cell will die based on the list sum, and if it already alive
-       cellRules numAliveNeighbours curCellAlive
+       -- Return whether the cell will live based on the list sum, and if it already alive
+       willCellLive numAliveNeighbours curCellAlive
    in
      map (\l -> (map (\((x,y),a) -> (cellLives gameStateIndexes (x,y))) l)) gameStateIndexes
     
 -- View
-view : (Int, Int) -> GameState -> Element
+view : Point -> GameState -> Element
 view (w,h) gameState =
   let 
     listSize = length gameState
